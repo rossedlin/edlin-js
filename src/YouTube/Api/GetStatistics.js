5bcf3753e80da1169247b609bb8cc0cdc353de63
Ross Edlin <rossedlin@gmail.com>
2023-09-18 19:54:41 +0100

    Refactr

--- src/YouTube/Api/GetStatistics.js
+++ src/YouTube/Api/GetStatistics.js
@@ -1,32 +1,38 @@
+/**
+ *
+ * @param id
+ * @param key
+ * @return {Promise<{subscribers: number, videos: number, views: number}>}
+ */
 module.exports = async function GetStatistics(id, key) {
 
   const postsRes = await fetch(
-    'https://www.googleapis.com/youtube/v3/channels?' + new URLSearchParams({
-      part: 'statistics',
-      id: id,
-      key: key,
-    }),
-    {
-      method:  'GET',
-      headers: {
-        'Content-Type':  'application/json',
+      'https://www.googleapis.com/youtube/v3/channels?' + new URLSearchParams({
+                                                          part: 'statistics',
+                                                          id:   id,
+                                                          key:  key,
+                                                        }),
+      {
+        method:  'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    },
   );
 
   const post = await postsRes.json();
 
   if (post['items'][0]['statistics']) {
     return {
-      views: Number(post['items'][0]['statistics']['viewCount']),
+      views:       Number(post['items'][0]['statistics']['viewCount']),
       subscribers: Number(post['items'][0]['statistics']['subscriberCount']),
-      videos: Number(post['items'][0]['statistics']['videoCount']),
-    }
+      videos:      Number(post['items'][0]['statistics']['videoCount']),
+    };
   }
 
   return {
-    views: 0,
+    views:       0,
     subscribers: 0,
-    videos: 0,
-  }
-}
+    videos:      0,
+  };
+};
