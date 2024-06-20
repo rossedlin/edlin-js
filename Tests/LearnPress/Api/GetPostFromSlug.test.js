de58017ea1d5452c210d1daa7cbc21d734554d01
Ross Edlin <rossedlin@gmail.com>
2024-06-20 15:05:58 +0100

    Add first unit test with Jest

---
+++ Tests/LearnPress/Api/GetPostFromSlug.test.js
@@ -0,0 +1,14 @@
+const GetPostFromSlug = require('../../../Headless/Api/GetPostFromSlug');
+
+test('string with a single number should result in the number itself', async () => {
+
+    post = await GetPostFromSlug('https://wp.codewithross.com/', 'stripe-laravel');
+
+    console.log(post);
+
+    expect(post).toMatchObject({
+        id: 2944,
+        status: 'publish',
+        slug: 'stripe-laravel'
+    });
+});
