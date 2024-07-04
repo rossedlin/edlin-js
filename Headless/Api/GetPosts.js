1a7e75dfa9887ecdebdd9b1550a5f3b1157031b0
Ross Edlin <rossedlin@gmail.com>
2024-07-04 15:18:22 +0100

    Add per_page arg

--- Headless/Api/GetPosts.js
+++ Headless/Api/GetPosts.js
@@ -4,7 +4,9 @@
  */
 module.exports = async function GetPosts(baseUrl, args = {}) {
     const draft = (args["draft"] !== undefined && args["draft"] === true) ? 1 : 0;
-    const postsRes = await fetch(baseUrl + '/wp-json/headless/v1/posts?draft=' + draft);
+    const per_page = (args["per_page"] !== undefined) ? args["per_page"] : 20;
+    const postsRes = await fetch(baseUrl + '/wp-json/headless/v1/posts?draft=' + draft
+        + '&per_page=' + per_page);
 
     return await postsRes.json();
 };
