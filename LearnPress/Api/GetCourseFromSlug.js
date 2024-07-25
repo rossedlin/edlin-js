6c3c5b567a11dfe053e5dcb903b8c8f13620ff85
Ross Edlin <rossedlin@gmail.com>
2024-07-25 17:34:31 +0100

    Add a working JSON Schema Validator

--- LearnPress/Api/GetCourseFromSlug.js
+++ LearnPress/Api/GetCourseFromSlug.js
@@ -1,10 +1,19 @@
-/**
+const {Draft2019} = require("json-schema-library");
+const schema = require('../Schema/GetCourses.json');
+
+/**s
  *
  * @return {Promise<any>}
  */
 module.exports = async function GetPageFromSlug(baseUrl, slug) {
-  const res = await fetch(baseUrl + '/wp-json/learnpress/v1/courses?slug=' + slug);
-  const courses = await res.json();
+    const res = await fetch(baseUrl + '/wp-json/learnpress/v1/courses?slug=' + slug);
+    const json = await res.json();
 
-  return courses[0];
+    const draft = new Draft2019(schema);
+    const errors = draft.validate(json);
+
+    if (errors.length) {
+        return null;
+    }
+    return json[0];
 };
